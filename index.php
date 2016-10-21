6990f14b373a8e7e0b6ac63992054eb5dc737479
John Walker <john@cofferings.co.uk>
2016-10-21 05:47:42 +0200

    Upload basic

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Slight re-org on the go..</title>
+        <style>
+            *{margin:auto;width:500px;margin-top:2em;text-align:center;}
+            h1{font-family:helvetica;font-size:1.2em;}
+            img{width:6em;}
+            ul{width:343px;}
+	    ul li img{width:3em;}
+	    ul li{list-style:none;float:left;width:initial;margin-right:1em;}
+        </style>
+    </head>
+    <body>
+        <img src="geeks-new.jpg">
+        <ul>
+ 	  <li><a href="https://instagram.com/coffeerings/"><img src="ins.png"></a></li>
+	  <li><a href="https://www.linkedin.com/profile/view?id=18955934"><img src="lin.png"></a></li>
+          <li><a href="https://twitter.com/coffeerings/"><img src="twi.png"></a></li>
+          <li><a href="https://facebook.com/coffeerings/"><img src="fac.png"></a></li>
+          <li><a href="https://flickr.com/photos/coffeerings/"><img src="fli.png"></a></li>
+        </ul> 
+    </body>
+</html>
